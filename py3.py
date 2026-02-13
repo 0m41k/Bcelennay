e21146a1133c01a74feb66a61c5697121021c8aa
Владимир <103315228+0m41k@users.noreply.github.com>
2026-02-13 22:30:19 +0500

    v2

--- py3.py
+++ py3.py
@@ -12,6 +12,7 @@ class GameSprite(pygame.sprite.Sprite):
 class Enemy(GameSprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, speed):
         super().__init__(player_image, player_x, player_y, size_x, size_y, speed)
+        self.original_image = pygame.image.load(player_image)
         self.angle = random.uniform(0, 360)
         self.start_size = (size_x, size_y)
         self.count = 0
@@ -23,15 +24,17 @@ class Enemy(GameSprite):
         if self.count >= 20:
             self.rect.width += 1
             self.rect.height += 1
-            self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+            self.image = pygame.transform.scale(self.original_image, (self.rect.width, self.rect.height))
             self.count = 0
         self.count += 1
         if self.rect.left < 0 or self.rect.right > window_width or self.rect.top < 0 or self.rect.bottom > window_height:
             self.rect.width, self.rect.height = self.start_size
-            self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
-            self.rect.x = random.randint(345,355)
-            self.rect.y = random.randint(245,255)
-            self.angle = random.uniform(0, 360)
+            self.image = pygame.transform.scale(self.original_image, (self.rect.width, self.rect.height))
+            # Респавн на периметре окружности
+            x, y, angle = get_spawn_position()
+            self.rect.x = x
+            self.rect.y = y
+            self.angle = angle
 
 pygame.init()
 
@@ -40,20 +43,38 @@ window_height = 500
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Сквозь вселенную')
 
-background = pygame.transform.scale(pygame.image.load('C:/Users/solda/Music/GitHub/Bcelennay/black.png'), (window_width, window_height))
+background = pygame.transform.scale(pygame.image.load('C:/Users/solda/OneDrive/Документы/Python/Bcelennay/black.png'), (window_width, window_height))
+
+# Параметры окружности для появления врагов
+center_x = window_width // 2
+center_y = window_height // 2
+spawn_radius = 5  # Радиус окружности для появления врагов
+
+def get_spawn_position():
+    """Возвращает случайную позицию на периметре окружности"""
+    angle = random.uniform(0, 360)
+    x = center_x + spawn_radius * math.cos(math.radians(angle))
+    y = center_y + spawn_radius * math.sin(math.radians(angle))
+    return x, y, angle
 
 enemy_size = 2
 enemy_speed = 4
 enemies = pygame.sprite.Group()
 
 for i in range(33):
-    enemy = Enemy('C:/Users/solda/Music/GitHub/Bcelennay/belo.jpg', random.randint(349,351), random.randint(249,251), enemy_size, enemy_size, enemy_speed)
+    x, y, angle = get_spawn_position()
+    enemy = Enemy('C:/Users/solda/OneDrive/Документы/Python/Bcelennay/belo.jpg', x, y, enemy_size, enemy_size, enemy_speed)
+    enemy.angle = angle
     enemies.add(enemy)
 for i in range(33):
-    enemy = Enemy('C:/Users/solda/Music/GitHub/Bcelennay/Purple.jpg', random.randint(349,351), random.randint(249,251), enemy_size, enemy_size, enemy_speed)
+    x, y, angle = get_spawn_position()
+    enemy = Enemy('C:/Users/solda/OneDrive/Документы/Python/Bcelennay/Purple.jpg', x, y, enemy_size, enemy_size, enemy_speed)
+    enemy.angle = angle
     enemies.add(enemy)
 for i in range(33):
-    enemy = Enemy('C:/Users/solda/Music/GitHub/Bcelennay/PurpleBlack.jpg', random.randint(349,351), random.randint(249,251), enemy_size, enemy_size, enemy_speed)
+    x, y, angle = get_spawn_position()
+    enemy = Enemy('C:/Users/solda/OneDrive/Документы/Python/Bcelennay/PurpleBlack.jpg', x, y, enemy_size, enemy_size, enemy_speed)
+    enemy.angle = angle
     enemies.add(enemy)
 
 clock = pygame.time.Clock()
